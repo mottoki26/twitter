aaa11205465bebbcbd3291b70da740bc3fcea0ac
motoki26 <92636420+mottoki26@users.noreply.github.com>
2022-05-23 14:01:46 +0900

    Delete dbConnection.php

--- common/dbConnection.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $dsn = 'mysql:host=us-cdbr-east-05.cleardb.net; dbname=heroku_2fa9c229c266d42; charset=utf8';
-    $dbuser = 'b7b0fec2387eb8';
-    $dbpass = 'e0318f9e';
-    $dbh = new PDO($dsn, $dbuser, $dbpass);
-    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
