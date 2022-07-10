f6bacf0e86732d5f03fc3a86cfe7f3d91ddf7901
motoki <motoki.1329@gmail.com>
2022-07-10 17:57:15 +0900

    erro

--- common/dbConnection.php
+++ common/dbConnection.php
@@ -1,8 +1,8 @@
 <?php
     $URL = getenv('URL');
     $dsn = 'mysql:host='.$URL.'; dbname=heroku_2fa9c229c266d42; charset=utf8';
-    $dbuser = getenv('USER');
-    $dbpass = getenv('PASS');
+    $dbuser = getenv('DBUSER');
+    $dbpass = getenv('DBPASS');
     error_log('URL:'.$URL);
     error_log('DSN:'.$dsn);
     error_log('USER:'.$dbuser);
