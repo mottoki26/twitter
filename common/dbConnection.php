cec12d2ae5b593ca30954984207434969b6794cd
motoki <motoki.1329@gmail.com>
2022-07-10 17:27:47 +0900

    env

--- common/dbConnection.php
+++ common/dbConnection.php
@@ -1,7 +1,8 @@
 <?php
-    $dsn = 'mysql:host=us-cdbr-east-05.cleardb.net; dbname=heroku_2fa9c229c266d42; charset=utf8';
-    $dbuser = 'b7b0fec2387eb8';
-    $dbpass = 'e0318f9e';
+    $URL = getenv('URL');
+    $dsn = 'mysql:host='.$URL.'; dbname=heroku_2fa9c229c266d42; charset=utf8';
+    $dbuser = getenv('USER');
+    $dbpass = getenv('PASS');
     $dbh = new PDO($dsn, $dbuser, $dbpass);
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
