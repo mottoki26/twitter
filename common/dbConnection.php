ca5f67c22916c64b1905e2bea6b2fe7ef84cabee
motoki <motoki.1329@gmail.com>
2022-07-10 17:51:09 +0900

    error

--- common/dbConnection.php
+++ common/dbConnection.php
@@ -3,6 +3,12 @@
     $dsn = 'mysql:host='.$URL.'; dbname=heroku_2fa9c229c266d42; charset=utf8';
     $dbuser = getenv('USER');
     $dbpass = getenv('PASS');
+    $datas['URL'] = $URL;
+    $datas['DSN'] = $dsn;
+    $datas['USER'] = $dbuser;
+    $datas['PASS'] = $dbpass;
+    error_log($datas);
+
     $dbh = new PDO($dsn, $dbuser, $dbpass);
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
