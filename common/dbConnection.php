1539c520af2976d7a3118fe253aaf8f09c24e27c
motoki <motoki.1329@gmail.com>
2022-07-10 17:58:18 +0900

    error confirm

--- common/dbConnection.php
+++ common/dbConnection.php
@@ -3,10 +3,10 @@
     $dsn = 'mysql:host='.$URL.'; dbname=heroku_2fa9c229c266d42; charset=utf8';
     $dbuser = getenv('DBUSER');
     $dbpass = getenv('DBPASS');
-    error_log('URL:'.$URL);
-    error_log('DSN:'.$dsn);
-    error_log('USER:'.$dbuser);
-    error_log('PASS:'.$dbpass);
+    // error_log('URL:'.$URL);
+    // error_log('DSN:'.$dsn);
+    // error_log('USER:'.$dbuser);
+    // error_log('PASS:'.$dbpass);
 
     $dbh = new PDO($dsn, $dbuser, $dbpass);
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
